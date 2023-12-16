bc57e3840f0be4ec5d63ed82e60de84a369df34a
Cornbread2100 <cornbread2100@cornbread2100.com>
2023-12-15 21:19:19 -0500

    free look

--- main.js
+++ main.js
@@ -1,15 +1,42 @@
 javascript:(() => {
   function buttonPressed(b) {
-    if (typeof b === 'object') {
-      return b.pressed;
-    }
-    return b === 1.0;
+    return typeof b == 'object' ? b.pressed : b == 1.0;
   }
 
 	simulation.mouseDistance = 40;
   simulation.mouseAngle = 0;
+  simulation.mousePos = { x: 0, y: 0 }
+  simulation.customLook = false;
 
+  simulation.camera = () => {
+    if (simulation.customLook) {
+      simulation.mouse.x = simulation.mousePos.x;
+      simulation.mouse.y = simulation.mousePos.y;
+   	} else {
+      var mCanvasPos = {
+        x: ((m.pos.x + m.transX - canvas.width2) /  simulation.edgeZoomOutSmooth) * simulation.zoom + canvas.width2,
+        y: simulation.mouse.y = ((m.pos.y + m.transY - canvas.height2) /  simulation.edgeZoomOutSmooth) * simulation.zoom + canvas.height2
+      }
+
+      simulation.mouse.x = mCanvasPos.x + Math.cos(simulation.mouseAngle) * simulation.mouseDistance;
+      simulation.mouse.y = mCanvasPos.y + Math.sin(simulation.mouseAngle) * simulation.mouseDistance;
+    }
+    
+    const dx = simulation.mouse.x / window.innerWidth - 0.5;
+    const dy = simulation.mouse.y / window.innerHeight - 0.5;
+    const d = Math.max(dx * dx, dy * dy)
+    simulation.edgeZoomOutSmooth = (1 + 4 * d * d) * 0.04 + simulation.edgeZoomOutSmooth * 0.96
+
+    ctx.save();
+    ctx.translate(canvas.width2, canvas.height2);
+    ctx.scale(simulation.zoom / simulation.edgeZoomOutSmooth, simulation.zoom / simulation.edgeZoomOutSmooth);
+    ctx.translate(-canvas.width2 + m.transX, -canvas.height2 + m.transY);
+    simulation.mouseInGame.x = (simulation.mouse.x - canvas.width2) / simulation.zoom * simulation.edgeZoomOutSmooth + canvas.width2 - m.transX;
+    simulation.mouseInGame.y = (simulation.mouse.y - canvas.height2) / simulation.zoom * simulation.edgeZoomOutSmooth + canvas.height2 - m.transY;
+  }
+  
   function setCrosshair() {
+    simulation.customLook = false;
     var mCanvasPos = {
       x: ((m.pos.x + m.transX - canvas.width2) /  simulation.edgeZoomOutSmooth) * simulation.zoom + canvas.width2,
       y: simulation.mouse.y = ((m.pos.y + m.transY - canvas.height2) /  simulation.edgeZoomOutSmooth) * simulation.zoom + canvas.height2
@@ -17,6 +44,16 @@ javascript:(() => {
 
     simulation.mouse.x = mCanvasPos.x + Math.cos(simulation.mouseAngle) * simulation.mouseDistance;
     simulation.mouse.y = mCanvasPos.y + Math.sin(simulation.mouseAngle) * simulation.mouseDistance;
+    simulation.mousePos = { x: simulation.mouse.x, y: simulation.mouse.y };
+    
+    simulation.mouseInGame.x = (simulation.mouse.x - canvas.width2) / simulation.zoom * simulation.edgeZoomOutSmooth + canvas.width2 - m.transX;
+    simulation.mouseInGame.y = (simulation.mouse.y - canvas.height2) / simulation.zoom * simulation.edgeZoomOutSmooth + canvas.height2 - m.transY;
+  }
+  
+  function setCrosshairPoint() {
+    simulation.customLook = true;
+    simulation.mouse.x = simulation.mousePos.x;
+    simulation.mouse.y = simulation.mousePos.y;
     
     simulation.mouseInGame.x = (simulation.mouse.x - canvas.width2) / simulation.zoom * simulation.edgeZoomOutSmooth + canvas.width2 - m.transX;
     simulation.mouseInGame.y = (simulation.mouse.y - canvas.height2) / simulation.zoom * simulation.edgeZoomOutSmooth + canvas.height2 - m.transY;
@@ -28,7 +65,8 @@ javascript:(() => {
   });
 
   var lastInputs = [];
-	var crouchToggled = false;
+  var crouchToggled = false;
+  var freeLook = false;
   function loop() {
     const gpInputs = [];
     const gamepad = navigator.getGamepads()[0];
@@ -58,6 +96,7 @@ javascript:(() => {
       if (gpInputs.includes(5) && !lastInputs.includes(5)) simulation.nextGun();
       input.field = gpInputs.includes(6);
       input.fire = gpInputs.includes(7);
+      if (gpInputs.includes(8) && !lastInputs.includes(8)) freeLook = !freeLook;
       if (gpInputs.includes(9) && !lastInputs.includes(9)) {
         if (!simulation.isChoosing && m.alive) {
           if (!tech.isNoDraftPause) {
@@ -88,10 +127,16 @@ javascript:(() => {
     }
     
     // joysticks
-    simulation.mouseDistance = Math.sqrt(gamepad.axes[2]**2 + gamepad.axes[3]**2) * 100;
-    if (simulation.mouseDistance < 40) simulation.mouseDistance = 40;
-    else simulation.mouseAngle = Math.atan2(gamepad.axes[3], gamepad.axes[2]);
-    setCrosshair();
+    if (freeLook) {
+      if (Math.abs(gamepad.axes[2]) > 0.3) simulation.mousePos.x += gamepad.axes[2] * 5;
+      if (Math.abs(gamepad.axes[3]) > 0.3) simulation.mousePos.y += gamepad.axes[3] * 5;
+      setCrosshairPoint();
+    } else {
+      simulation.mouseDistance = Math.sqrt(gamepad.axes[2]**2 + gamepad.axes[3]**2) * 300;
+      if (simulation.mouseDistance < 120) simulation.mouseDistance = 120;
+      else simulation.mouseAngle = Math.atan2(gamepad.axes[3], gamepad.axes[2]);
+      setCrosshair();
+    }
     
     const moveDistance = Math.sqrt(gamepad.axes[1]**2 + gamepad.axes[0]**2);
     const moveAngle = Math.atan2(gamepad.axes[1], gamepad.axes[0]);
@@ -107,8 +152,7 @@ javascript:(() => {
     }
     
 
-//     if (gpInputs.length > 0) console.log(gpInputs, input.up);
-    console.log(crouchToggled);
+//     if (gpInputs.length > 0) console.log(gpInputs);
 
     lastInputs = gpInputs;
     requestAnimationFrame(loop);
